a2e9685d01da5e9f6679a5107b21c50060d58a28
Felipe Alcantra de Assis <lipao01017@gmail.com>
2026-08-11 17:02:01 -0300

    Inicio da lista 1 POO

---
+++ 2periodo/POO/Lista 001 Resolucao/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n1 = sc.nextDouble();
+        double n2 = sc.nextDouble();
+        double resultado = n1 + n2;
+        System.out.println(resultado);
+    }
+}
